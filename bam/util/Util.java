3b069844f6ea779fe7e4a497e348ce0e1655119b
shinmj0255 <shinmj0255@gmail.com>
2023-04-17 19:35:45 +0900

    패키지 분리,핵심로직 이전

--- bam/util/Util.java
+++ bam/util/Util.java
@@ -3,12 +3,12 @@ package bam.util;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-	public class Util {
-		public static String getDateStr() {
-			SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+public class Util {
+	public static String getDateStr() {
+		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-			Date now = new Date();
+		Date now = new Date();
 
-			return sdf1.format(now);
-		}
+		return sdf1.format(now);
 	}
+}
