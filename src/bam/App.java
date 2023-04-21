ad528c34651c822aac3a9b78942ff1ae5f72a8ba
shinmj0255 <shinmj0255@gmail.com>
2023-04-21 21:18:28 +0900

    MemberService/Dao,ArticleService/Dao

--- src/bam/App.java
+++ src/bam/App.java
@@ -1,32 +1,21 @@
 package bam;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 import bam.controller.ArticleController;
 import bam.controller.Controller;
 import bam.controller.MemberController;
-import bam.dto.Article;
-import bam.dto.Member;
 
 public class App {
-	private List<Article> articles;
-	private List<Member> members;
-	
-	public App() {
-		articles = new ArrayList<>();
-		members = new ArrayList<>();
-	}
-	
+
 	public void run() {
 		System.out.println("== 프로그램 시작 ==");
 
 		Scanner sc = new Scanner(System.in);
 
-		MemberController memberController = new MemberController(members, sc);
-		ArticleController articleController = new ArticleController(articles, sc);
-		
+		MemberController memberController = new MemberController(sc);
+		ArticleController articleController = new ArticleController(sc);
+
 		articleController.makeTestData();
 		memberController.makeTestData();
 
@@ -39,19 +28,18 @@ public class App {
 			}
 
 			String[] cmdBits = cmd.split(" ");
-			
-			if(cmdBits.length == 1) {
+
+			if (cmdBits.length == 1) {
 				System.out.println("명령어를 확인해주세요");
 				continue;
 			}
-			
+
 			String controllerName = cmdBits[0];
 			String methodName = cmdBits[1];
-			
+
 			Controller controller = null;
-			
-			
-			if(controllerName.equals("article")) {
+
+			if (controllerName.equals("article")) {
 				controller = articleController;
 			} else if (controllerName.equals("member")) {
 				controller = memberController;
@@ -59,28 +47,30 @@ public class App {
 				System.out.println("존재하지 않는 명령어 입니다");
 				continue;
 			}
-			
-			switch(methodName) {
-			case "logout":
-			case "write":	
-			case "modify":
-			case "delete":
+
+			String actionName = controllerName + "/" + methodName;
+
+			switch (actionName) {
+			case "article/write":
+			case "article/modify":
+			case "article/delete":
+			case "member/logout":
 				if (Controller.loginedMember == null) {
 					System.out.println("로그인 상태가 아닙니다");
 					continue;
 				}
 				break;
-			case "join":
-			case "login":
+			case "member/join":
+			case "member/login":
 				if (Controller.loginedMember != null) {
 					System.out.println("로그아웃 후 이용해주세요");
 					continue;
 				}
 				break;
 			}
-			
+
 			controller.doAction(cmd, methodName);
-			
+
 		}
 
 		sc.close();
